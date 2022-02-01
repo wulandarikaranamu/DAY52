9e9d01ae5459b2137766108e83786ed5a0da73f0
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-02-01 19:41:17 +0700

    komentar

---
+++ day52.py
@@ -0,0 +1,34 @@
+#merry wulandari
+
+buah1 = "apel"
+buah2 = "semangka"
+
+print("[proses awal]")
+print(f'buah1 = {buah1}, buah2 = {buah2}\n')#cara menukar dua variabel
+
+print("="*30)
+
+buah1, buah2 = buah2, buah1
+
+print("[proses akhir]")
+print(f'buah1 = {buah1}, buah2 = {buah2}')
+
+print("="*30)
+
+angka1 = 2
+angka2 = 3
+
+print("[nilai awal]")
+print(f'angka1 = {angka1}, angka2 = {angka2}\n')#cara menukar dua variabel dengan operator aritmatika
+
+angka1 = angka1 + angka2
+angka2 = angka1 - angka2
+angka1 = angka1 - angka2
+
+print("[nilai awal]")
+print(f'angka1 = {angka1}, angka2 = {angka2}')
+
+
+
+
+
